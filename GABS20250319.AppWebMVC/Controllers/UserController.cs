b3a0ad65adb0c78b074b913f33547b75116ab4e8
Gerardo <ge6975507@gmailcom>
2025-03-20 21:42:28 -0600

    autenticacion de admin y gerente y creacion de la vista de login en  iniciar secion

--- GABS20250319.AppWebMVC/Controllers/UserController.cs
+++ GABS20250319.AppWebMVC/Controllers/UserController.cs
@@ -6,6 +6,12 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using GABS20250319.AppWebMVC.Models;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using System.Security.Cryptography;
+using System.Text;
+using System.Security.Claims;
 
 namespace GABS20250319.AppWebMVC.Controllers
 {
@@ -64,6 +70,47 @@ namespace GABS20250319.AppWebMVC.Controllers
             return View(user);
         }
 
+        [AllowAnonymous]
+        public async Task<IActionResult> CerrarSession()
+        {
+            // Hola mundo
+            await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            return RedirectToAction("Index", "Home");
+        }
+
+        [AllowAnonymous]
+        public IActionResult Login()
+        {
+            return View();
+        }
+        [AllowAnonymous]
+        [HttpPost]
+
+        public async Task<IActionResult> Login(User users)
+        {
+            users.PasswordHash = CalcularHashMD5(users.PasswordHash);
+            var usuarioAuth = await _context.
+                Users.
+                FirstOrDefaultAsync(s => s.Email == users.Email && s.PasswordHash == users.PasswordHash);
+            if (usuarioAuth != null && usuarioAuth.UserId > 0 && usuarioAuth.Email == users.Email)
+            {
+                var claims = new[] {
+                    new Claim(ClaimTypes.Name, usuarioAuth.Email),
+                    new Claim("UsersId", usuarioAuth.UserId.ToString()),
+                     new Claim("UserName", usuarioAuth.Username),
+                    new Claim(ClaimTypes.Role, usuarioAuth.Role)
+                    };
+                var identity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+                await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(identity));
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                ModelState.AddModelError("", "El email o contraseña estan incorrectos");
+                return View();
+            }
+        }
+
         // GET: User/Edit/5
         public async Task<IActionResult> Edit(int? id)
         {
@@ -92,12 +139,18 @@ namespace GABS20250319.AppWebMVC.Controllers
                 return NotFound();
             }
 
-            if (ModelState.IsValid)
+            var usuarioUpdate = await _context.Users
+              .FirstOrDefaultAsync(m => m.UserId == user.UserId);
             {
                 try
                 {
+
+                    usuarioUpdate.Username = user.Username;
+                    usuarioUpdate.Email = user.Email;
+                    usuarioUpdate.Role = user.Role;
                     _context.Update(user);
                     await _context.SaveChangesAsync();
+                    return RedirectToAction(nameof(Index));
                 }
                 catch (DbUpdateConcurrencyException)
                 {
@@ -107,12 +160,12 @@ namespace GABS20250319.AppWebMVC.Controllers
                     }
                     else
                     {
-                        throw;
+                        return View(user);
                     }
                 }
-                return RedirectToAction(nameof(Index));
+                
             }
-            return View(user);
+            
         }
 
         // GET: User/Delete/5
@@ -152,5 +205,62 @@ namespace GABS20250319.AppWebMVC.Controllers
         {
             return _context.Users.Any(e => e.UserId == id);
         }
+
+        public async Task<IActionResult> Perfil()
+        {
+
+            var idStr = User.FindFirst("Id")?.Value;
+            int id = int.Parse(idStr);
+            var usuario = await _context.Users.FindAsync(id);
+            return View(usuario);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Perfil(int id, [Bind("Id,Nombre,Email,Estatus,Rol")] User usuario)
+        {
+            if (id != usuario.UserId)
+            {
+                return NotFound();
+            }
+            var usuarioUpdate = await _context.Users
+                 .FirstOrDefaultAsync(m => m.UserId == usuario.UserId);
+            try
+            {
+                usuarioUpdate.Username = usuario.Username;
+                usuarioUpdate.Email = usuario.Email;
+                usuarioUpdate.Role = usuario.Role;
+                _context.Update(usuarioUpdate);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index", "Home");
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!UserExists(usuario.UserId))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    return View(usuario);
+                }
+            }
+        }
+        private string CalcularHashMD5(string input)
+        {
+            using (MD5 md5 = MD5.Create())
+            {
+                byte[] inputBytes = Encoding.UTF8.GetBytes(input);
+                byte[] hashBytes = md5.ComputeHash(inputBytes);
+
+                StringBuilder sb = new StringBuilder();
+                for (int i = 0; i < hashBytes.Length; i++)
+                {
+                    sb.Append(hashBytes[i].ToString("x2")); // "x2" convierte el byte en una cadena hexadecimal de dos caracteres.
+                }
+                return sb.ToString();
+            }
+        }
+
     }
 }
